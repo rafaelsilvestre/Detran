e95fa0ec2ebc09a6b09ae43e60fe0ac389b234c2
Rafael Silvestre <rafaelsilvestre.1@hotmail.com>
2017-05-11 14:48:31 -0300

    Interface Responder

--- src/view/Main.java
+++ src/view/Main.java
@@ -1,9 +1,23 @@
 package view;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-	public static void main(String[] args) {
-		
+public class Main extends Application{
 
-	}
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent parent = FXMLLoader.load(getClass().getResource("ResponderProva.fxml"));
+        Scene scene = new Scene(parent);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
+
